17ac057e13d2079e3c2b3e48dbb5e976a73d4fef
PurnimaMahato <purnimamahato782@gmail.com>
2023-06-30 17:38:46 +0530

    Countdown timer

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const startBtn = document.querySelector(".play button");
+
+function countdown() {
+    var countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
+
+    var x = setInterval(function () {
+
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        document.getElementById("day").innerHTML = days;
+        document.getElementById("hour").innerHTML = hours;
+        document.getElementById("min").innerHTML = minutes;
+        document.getElementById("sec").innerHTML = seconds;
+
+    }, 1000);
+
+}
+
+startBtn.addEventListener("click", () => {
+
+    countdown();
+
+}) 
